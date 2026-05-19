aaa7da98ee5ba8356b4b7ec9517e833f3cc5b988
valecdlr6698 <2025471@student.cct.ie>
2026-05-19 09:04:54 +0100

    test

---
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/SchoolEmployeeSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.schoolemployeesystem;
+
+/**
+ *
+ * @author valec
+ */
+public class SchoolEmployeeSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
