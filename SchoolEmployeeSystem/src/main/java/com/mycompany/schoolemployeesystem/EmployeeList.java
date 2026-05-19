822d5311aad20892cf04aa0ed672eb6f957897a7
valecdlr6698 <2025471@student.cct.ie>
2026-05-19 20:47:36 +0100

    Created EmployeeList with fixed array and addEmployee method

--- SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/EmployeeList.java
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/EmployeeList.java
@@ -5,9 +5,34 @@
 package com.mycompany.schoolemployeesystem;
 
 /**
- *
+ * Stores employees using a fixed-size array of 50 elements.
+ * Provides basic operations such as add, check full and check empty.
  * @author valec
  */
 public class EmployeeList {
+    
+    Employee[] employees = new Employee[20];
+    int count = 0;
+
+    //Checks if the array is full.
+    public boolean isFull() {
+        return count == 20;
+    }
+
+    //Checks if the array is empty.
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    //Adds a new employee to the list if space is available.
+    public void addEmployee(Employee e) {
+        if (isFull()) {
+            System.out.println("Storage full. Cannot add more employees.");
+            return;
+        }
+        employees[count] = e;
+        count++;
+        System.out.println("Employee added: " + e.name);
+    }
     
 }
