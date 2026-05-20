762601efdb62963e727b92dcb6d19ae3bc9c3bc5
valecdlr6698 <2025471@student.cct.ie>
2026-05-20 07:43:08 +0100

    Department class created

---
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Department.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.schoolemployeesystem;
+
+/**
+ *
+ * @author valec
+ */
+public class Department {
+    
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    
+}
