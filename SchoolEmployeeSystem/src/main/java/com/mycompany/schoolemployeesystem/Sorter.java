50401653d091266bfde4e54e0561661c2524b612
valecdlr6698 <2025471@student.cct.ie>
2026-05-20 13:25:43 +0100

    Added recursive sorting algorithm (Sorter class)

--- SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Sorter.java
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Sorter.java
@@ -5,9 +5,33 @@
 package com.mycompany.schoolemployeesystem;
 
 /**
- *
+ * Provides a recursive sorting algorithm (recursive insertion sort).
+ * Sorts an array of Strings alphabetically.
  * @author valec
  */
 public class Sorter {
     
+    // Recursively sorts the first n elements of the array.
+    public static void recursiveSort(String[] arr, int n) {
+
+        // Base case: array of size 1 is already sorted
+        if (n == 1) {
+            return;
+        }
+
+        // Sort first n-1 elements recursively
+        recursiveSort(arr, n - 1);
+
+        // Insert last element into the sorted part
+        String last = arr[n - 1];
+        int j = n - 2;
+
+        // Move elements that are greater than 'last'
+        while (j >= 0 && arr[j].compareToIgnoreCase(last) > 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = last;
+    }    
 }
