5921cbcadf77c35a8935d045c7b593f708907940
valecdlr6698 <2025471@student.cct.ie>
2026-05-21 19:02:54 +0100

    Validator class created to provide simple validation methods for employee input fields

--- SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Validator.java
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Validator.java
@@ -5,9 +5,41 @@
 package com.mycompany.schoolemployeesystem;
 
 /**
- *
+ * Provides simple validation methods for employee input fields.
  * @author valec
  */
 public class Validator {
     
+    //Validates that the name is not empty.
+    public static boolean validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+        return false;
+    }
+
+    // Letters and spaces only including accent marks.
+    if (!name.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
+        return false;
+    }
+
+    
+    String clean = name.replace(" ", "");
+
+    // 2 letters minimum
+    return clean.length() >= 2;
+    }    
+
+    //Validates allowed manager types.
+    public static boolean validateManagerType(String type) {
+        return type.equalsIgnoreCase("Manager") ||
+               type.equalsIgnoreCase("Assistant") ||
+               type.equalsIgnoreCase("Coordinator");
+    }
+
+    //Validates allowed departments.
+    public static boolean validateDepartment(String dept) {
+        return dept.equalsIgnoreCase("Math") ||
+               dept.equalsIgnoreCase("Science") ||
+               dept.equalsIgnoreCase("English") ||
+               dept.equalsIgnoreCase("Sports");
+    }
 }
