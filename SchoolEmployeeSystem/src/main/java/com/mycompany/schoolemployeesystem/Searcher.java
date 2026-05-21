052d05a8881a089222c33300f0dc9205a92b6b3a
valecdlr6698 <2025471@student.cct.ie>
2026-05-21 08:54:31 +0100

    Added recursive search algorithm

--- SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Searcher.java
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/Searcher.java
@@ -10,4 +10,24 @@ package com.mycompany.schoolemployeesystem;
  */
 public class Searcher {
     
+    public static int recursiveSearch(Employee[] arr, String target, int left, int right) {
+
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+
+        int compare = arr[mid].name.compareToIgnoreCase(target);
+
+        if (compare == 0) {
+            return mid;
+        }
+
+        if (compare > 0) {
+            return recursiveSearch(arr, target, left, mid - 1);
+        } else {
+            return recursiveSearch(arr, target, mid + 1, right);
+        }
+    }    
 }
