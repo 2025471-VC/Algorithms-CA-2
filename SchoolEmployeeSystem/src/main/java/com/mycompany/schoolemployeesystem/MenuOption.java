383a73973ffe00dd196534a512435b55f34335a9
valecdlr6698 <2025471@student.cct.ie>
2026-05-22 10:19:33 +0100

    MenuOption class created

---
+++ SchoolEmployeeSystem/src/main/java/com/mycompany/schoolemployeesystem/MenuOption.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.schoolemployeesystem;
+
+/**
+ *
+ * @author valec
+ */
+public enum MenuOption {
+    
+    ADD_EMPLOYEE,
+    SORT_APPLICANTS,
+    SEARCH_EMPLOYEE,
+    BUILD_HIERARCHY,
+    EXIT
+    
+}
